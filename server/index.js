5bd825de643d0f0342ad5f389fe28546dc6dbcd4
Hemanth Kumar Gajavelli <hemanthceps@gmail.com>
2024-06-22 02:36:01 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import { fileURLToPath } from 'url';
 const app = express();
 
 app.use(cors({
-    origin: [""],
+    origin: ["https://zengpt-client.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }))
