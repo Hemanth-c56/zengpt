f3f919caeeda40cbb8d3094ace5c5a99b8279041
Hemanth Kumar Gajavelli <hemanthceps@gmail.com>
2024-06-22 09:48:52 +0530

    Update index.js 4

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import { fileURLToPath } from 'url';
 const app = express();
 
 app.use(cors({
-    origin: ["https://zengpt-client.vercel.app"],
+    origin: ["https://hermits-zen.netlify.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }))
